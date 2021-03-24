9bdedcf71a88508cfede2cf04256ffb457543ac4
J <xuyj0823@163.com>
2021-03-24 18:08:35 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("haha");
-        System.out.println();
+        System.out.println("heihei");
     }
 
 }
